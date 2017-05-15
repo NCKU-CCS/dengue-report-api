2d891ef9cf330975373b44285e77e847634249d1
Ching <dino8556@gmail.com>
2017-05-16 01:29:58 +0800

    add modle of bucketStatistics

--- bucketStatistics/models.py
+++ bucketStatistics/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class BucketStatistics(models.Model):
+    # 調查日期
+    investigate_date = models.DateField()
+    # 縣市、區、里
+    county = models.TextField()
+    town = models.TextField()
+    village = models.TextField()
+    # 統計卵數
+    total_egg_count = models.IntegerField(default=0)
+    # 陽性率
+    positive_rate = models.FloatField(default=0)
--- dengue_report_server/settings.py
+++ dengue_report_server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'bucket',
     'bucketRecord',
+    'bucketStatistics',
 ]
 
 MIDDLEWARE = [
